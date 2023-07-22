b658333b9f9dfdc699987dafa658ed8f4c973e68
gang_dramma <your_email@kottaholakottahola@gmail.com>
2023-07-22 21:10:10 +0500

    add a lot of styles and add styles for li label input

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 
-// const nav = () =>{ 
-//     const links = document.querySelector(".nav-links");
-//     const menu = document.querySelector(".menu");
-//     const navlinks = document.querySelectorAll(".nav-links li");
-//     menu.addEventListener("click",()=>{ 
-//         links.classList.toggle("active");
-//         navlinks.forEach((link,index)=>{ 
-//             if(link.style.animation){ 
-//                 link.style.animation = "";
-//             }
-//             else { 
-//                 link.style.animation = `navanim 0.5s ease forwards ${index / 7 + 0.01}s`;
-//             }
-//         });
-//         menu.classList.toggle("toggle");
-//     });
-// }
-// nav();
+const nav = () =>{ 
+    const links = document.querySelector(".nav-links");
+    const menu = document.querySelector(".menu");
+    const navlinks = document.querySelectorAll(".nav-links li");
+    menu.addEventListener("click",()=>{ 
+        links.classList.toggle("active");
+        navlinks.forEach((link,index)=>{ 
+            if(link.style.animation){ 
+                link.style.animation = "";
+            }
+            else { 
+                link.style.animation = `navanim 0.5s ease forwards ${index / 7 + 0.01}s`;
+            }
+        });
+        menu.classList.toggle("toggle");
+    });
+}
+nav();
 const checkbox = document.getElementById("checkbox")
+
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark")
 })
